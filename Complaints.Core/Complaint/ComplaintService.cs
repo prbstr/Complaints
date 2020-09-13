6e1de8e300a5f70b21646b235c60e5ac276e8613
parbster <parbster@gmail.com>
2020-09-13 13:42:56 +0200

    Add complaint service. Add complaint entity class.

---
+++ Complaints.Core/Complaint/ComplaintService.cs
@@ -0,0 +1,39 @@
+﻿using Complaints.Data.Contexts;
+using Complaints.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Complaints.Core.Complaint
+{
+    public interface IComplaintService
+    {
+        ComplaintEntity AddComplaint(ComplaintEntity complaint);
+        IEnumerable<ComplaintEntity> GetAll();
+        ComplaintEntity GetComplaintById(int id);
+    }
+
+    public class ComplaintService : IComplaintService
+    {
+        private ComplaintsContext _context;
+        public ComplaintService(ComplaintsContext context)
+        {
+            _context = context;
+        }
+
+        public ComplaintEntity AddComplaint(ComplaintEntity complaint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ComplaintEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ComplaintEntity GetComplaintById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
