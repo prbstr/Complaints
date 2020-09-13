aa0d60438943863b17cee2e6666a85a138f114a9
parbster <parbster@gmail.com>
2020-09-13 10:38:25 +0200

    Add users controller with get all users endpoint. Add complaints context to user service.

---
+++ Complaints.Api/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Complaints.Core.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Complaints.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+    }
+}
--- Complaints.Core/User/UserService.cs
+++ Complaints.Core/User/UserService.cs
@@ -1,15 +1,25 @@
-﻿using Complaints.Data.Entities;
+﻿using Complaints.Data.Contexts;
+using Complaints.Data.Entities;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Complaints.Core.User
 {
     public interface IUserService
     {
+        IEnumerable<UserEntity> GetAll();
         UserEntity Authenticate(string username, string password);
         UserEntity Create(UserEntity user, string password);
     }
+
     public class UserService : IUserService
     {
+        private ComplaintsContext _context;
+        public UserService(ComplaintsContext context)
+        {
+            _context = context;
+        }
+
         public UserEntity Authenticate(string username, string password)
         {
             throw new System.NotImplementedException();
@@ -18,6 +28,11 @@ namespace Complaints.Core.User
         public UserEntity Create(UserEntity user, string password)
         {
             throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<UserEntity> GetAll()
+        {
+            return _context.Users.ToList();   
         }
     }
 }
