bb6c1954eadeaba60e28359116356c59b17a25d9
parbster <parbster@gmail.com>
2020-09-13 11:22:41 +0200

    Add xUnit unit testing project. Inject inmemory database of complaints context. Add failing CreateUser test.

---
+++ Complaints.UnitTests/Services/UserServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using Complaints.Core.User;
+using Complaints.Data.Contexts;
+using Complaints.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using Xunit;
+
+namespace Complaints.UnitTests.Services
+{
+    public class DbFixture
+    {
+        public ServiceProvider ServiceProvider { get; private set; }
+        public DbFixture()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<ComplaintsContext>(options =>
+                options.UseInMemoryDatabase("testDb"),
+                ServiceLifetime.Transient
+            );
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+        }
+    }
+
+    public class UserServiceTests : IClassFixture<DbFixture>
+    {
+        private readonly ServiceProvider _serviceProvider;
+        public UserServiceTests(DbFixture fixture)
+        {
+            _serviceProvider = fixture.ServiceProvider;
+        }
+
+        [Theory]
+        [InlineData("name", "surname", "username", "password")]
+        public void ShouldCreateNewUserInInMemoryDatabase(string firstName, string lastName, string username, string password)
+        {
+            using var context = _serviceProvider.GetService<ComplaintsContext>();
+            var _userService = new UserService(context);
+            var userEntity = new UserEntity
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Username = username
+            };
+
+            var createdUser = _userService.Create(userEntity, password);
+
+            var userInDb = _userService.GetAll().First();
+            Assert.Equal(createdUser, userInDb);
+        }
+    }
+}
