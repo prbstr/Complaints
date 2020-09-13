bdaf0d1c9af137e0ef25341db991d71356e51d9a
parbster <parbster@gmail.com>
2020-09-13 10:19:53 +0200

    Add user entity and skeleton user service

---
+++ Complaints.Core/User/UserService.cs
@@ -0,0 +1,23 @@
+﻿using Complaints.Data.Entities;
+using System.Collections.Generic;
+
+namespace Complaints.Core.User
+{
+    public interface IUserService
+    {
+        UserEntity Authenticate(string username, string password);
+        UserEntity Create(UserEntity user, string password);
+    }
+    public class UserService : IUserService
+    {
+        public UserEntity Authenticate(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public UserEntity Create(UserEntity user, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
