47c3e7d11727c707c393ce4c3f198ce96f9eadfa
parbster <parbster@gmail.com>
2020-09-13 12:08:02 +0200

    Add authentication exception models. Add failing tests that expect authentication exceptions to be thrown.

---
+++ Complaints.Data/ViewModels/ExceptionViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Complaints.Data.ViewModels
+{
+    public class AuthenticationException : Exception
+    {
+        public AuthenticationException() : base()
+        {
+        }
+
+        public AuthenticationException(string message) : base(message)
+        {
+        }
+
+        public AuthenticationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- Complaints.UnitTests/Services/UserServiceTests.cs
+++ Complaints.UnitTests/Services/UserServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using Complaints.Core.User;
 using Complaints.Data.Contexts;
 using Complaints.Data.Entities;
+using Complaints.Data.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -53,6 +54,47 @@ namespace Complaints.UnitTests.Services
 
             var userInDb = _userService.GetAll().First();
             Assert.Equal(createdUser, userInDb);
+        }
+
+        [Theory]
+        [InlineData("name", "surname", "username", "")]
+        [InlineData("name", "surname", "username", null)]
+        public void ShouldThrowAnAuthenticationExceptionIfPasswordIsEmpty(string firstName, string lastName, string username, string password)
+        {
+            using var context = _serviceProvider.GetService<ComplaintsContext>();
+            var _userService = new UserService(context);
+            var userEntity = new UserEntity
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Username = username
+            };
+
+            Assert.Throws<AuthenticationException>(() => _userService.Create(userEntity, password));
+        }
+
+        [Theory]
+        [InlineData("name", "surname", "username", "password")]
+        public void ShouldThrowAnExceptionIfUsernameIsTaken(string firstName, string lastName, string username, string password)
+        {
+            using var context = _serviceProvider.GetService<ComplaintsContext>();
+            var _userService = new UserService(context);
+            var userEntity1 = new UserEntity
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Username = username
+            };
+
+            var userEntity2 = new UserEntity
+            {
+                FirstName = "name",
+                LastName = "surname",
+                Username = "username"
+            };
+
+            var user1 = _userService.Create(userEntity1, password);
+            Assert.Throws<AuthenticationException>(() => _userService.Create(userEntity2, password));
         }
     }
 }
